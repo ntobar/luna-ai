7c322e1eb8185f1db6255cc58cd8eb6231b059e3
Nicolas Tobar <ntobar@bluecedar.com>
2023-06-09 20:14:48 -0400

    added POST api

--- pages/api/incoming.js
+++ pages/api/incoming.js
@@ -1,34 +1,70 @@
 // api/incoming.js
 const axios = require('axios');
-require('dotenv').config();
+import axios from 'axios';
 
-module.exports = async (req, res) => {
+// This is an API route in Next.js
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
     // This is where you handle incoming messages
     const incomingMessage = req.body.Body;
-    console.log(`PROMPT IS: `, req);
     const fromNumber = req.body.From;
     
     // Generate a response using OpenAI's GPT-3
     const gpt3Response = await getGpt3Response(incomingMessage);
     
-    // Send a response back to Twilio
+    // Send a response back
     res.setHeader('Content-Type', 'text/xml');
-    res.send(`<Response><Message>${gpt3Response}</Message></Response>`);
-};
+    res.status(200).send(`<Response><Message>${gpt3Response}</Message></Response>`);
+  } else {
+    // Handle any other HTTP method
+    res.setHeader('Allow', ['POST']);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+}
 
 // Define the function to generate a response from OpenAi GPT-3
 async function getGpt3Response(prompt) {
-    console.log(prompt);
-    const response = await axios.post('https://api.openai.com/v1/engines/text-davinci-002/completions', {
-        prompt: prompt,
-        max_tokens: 60
-    }, {
-        headers: {
-            'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
-        }
-    });
+  const response = await axios.post('https://api.openai.com/v1/engines/davinci-codex/completions', {
+    prompt: prompt,
+    max_tokens: 60
+  }, {
+    headers: {
+      'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
+    }
+  });
+
+  return response.data.choices[0].text.trim();
+}
+
+// require('dotenv').config();
+
+// module.exports = async (req, res) => {
+//     // This is where you handle incoming messages
+//     const incomingMessage = req.body.Body;
+//     console.log(`PROMPT IS: `, req);
+//     const fromNumber = req.body.From;
+    
+//     // Generate a response using OpenAI's GPT-3
+//     const gpt3Response = await getGpt3Response(incomingMessage);
+    
+//     // Send a response back to Twilio
+//     res.setHeader('Content-Type', 'text/xml');
+//     res.send(`<Response><Message>${gpt3Response}</Message></Response>`);
+// };
+
+// // Define the function to generate a response from OpenAi GPT-3
+// async function getGpt3Response(prompt) {
+//     console.log(prompt);
+//     const response = await axios.post('https://api.openai.com/v1/engines/text-davinci-002/completions', {
+//         prompt: prompt,
+//         max_tokens: 60
+//     }, {
+//         headers: {
+//             'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
+//         }
+//     });
 
     
-    console.log(response.data.choices);
-    return response.data.choices[0].text.trim();
-}
+//     console.log(response.data.choices);
+//     return response.data.choices[0].text.trim();
+// }
