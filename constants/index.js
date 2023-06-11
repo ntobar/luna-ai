d5d6fa7e99fa36dcdca5562d040df681d5d58870
Nicolas Tobar <ntobar@bluecedar.com>
2023-06-11 19:01:05 -0400

    added landing page

---
+++ constants/index.js
@@ -0,0 +1,121 @@
+// export const exploreWorlds = [
+//   {
+//     id: 'world-1',
+//     imgUrl: '/planet-01.png',
+//     title: 'The Hogwarts',
+//   },
+//   {
+//     id: 'world-2',
+//     imgUrl: '/planet-02.png',
+//     title: 'The Upside Down',
+//   },
+//   {
+//     id: 'world-3',
+//     imgUrl: '/planet-03.png',
+//     title: 'Kadirojo Permai',
+//   },
+//   {
+//     id: 'world-4',
+//     imgUrl: '/planet-04.png',
+//     title: 'Paradise Island',
+//   },
+//   {
+//     id: 'world-5',
+//     imgUrl: '/planet-05.png',
+//     title: 'Hawkins Labs',
+//   },
+// ];
+export const exploreWorlds = [
+  {
+    id: 'world-1',
+    imgUrl: '/planet-01.png',
+    title: 'The Digital Metropolis',
+    description: 'A universe of information, just a message away',
+  },
+  {
+    id: 'world-2',
+    imgUrl: '/planet-02.png',
+    title: 'The Command Center',
+    description: 'Automated messaging at its finest',
+  },
+  {
+    id: 'world-3',
+    imgUrl: '/planet-03.png',
+    title: 'The Library',
+    description: 'Knowledge at your fingertips',
+  },
+  {
+    id: 'world-4',
+    imgUrl: '/planet-04.png',
+    title: 'The Concierge',
+    description: 'Personalized attention, AI-powered.',
+  },
+  {
+    id: 'world-5',
+    imgUrl: '/planet-05.png',
+    title: 'The Studio',
+    description: 'Craft imagery with AI precision, straight from your chat',
+  },
+];
+
+
+export const startingFeatures = [
+  `Simply add Luna's contact number to your WhatsApp contacts`,
+  `Start a conversation with Luna right away - there's no complex setup or training period. Luna is ready to assist from the moment you say hello`,
+  `Adjust Luna's level of interaction and access according to your preferences, making it a truly personalized chat assistant`,
+];
+
+export const newFeatures = [
+  {
+    imgUrl: '/vrpano.svg',
+    title: 'Powered by GPT-4',
+    subtitle:
+        `Luna is one of the first chat assistants to leverage the power of GPT-4`,
+  },
+  {
+    imgUrl: '/headset.svg',
+    title: 'Dynamic Learning',
+    subtitle:
+        `Luna.ai's unique design allows it to learn from your conversations`,
+  },
+];
+
+export const insights = [
+  {
+    imgUrl: '/planet-06.png',
+    title: 'Intelligent Assistance',
+    subtitle:
+        'Leveraging natural language processing and machine learning, Luna.ai delivers contextually accurate and timely responses for efficient chat management.',
+  },
+  {
+    imgUrl: '/planet-07.png',
+    title: 'Adaptive Learning',
+    subtitle:
+        'Luna.ai continually learns from conversations, improving its responses and interactions over time, providing a personalized chat experience.',
+  },
+  {
+    imgUrl: '/planet-08.png',
+    title: 'Customizable Interaction',
+    subtitle:
+        `Luna.ai's level of interaction can be tailored to user preferences, ranging from sporadic recommendations to responding only when addressed directly, for a truly personalized chat experience.`,
+  },
+];
+
+export const socials = [
+  {
+    name: 'twitter',
+    url: '/twitter.svg',
+  },
+  {
+    name: 'linkedin',
+    url: '/linkedin.svg',
+  },
+  {
+    name: 'instagram',
+    url: '/instagram.svg',
+  },
+  {
+    name: 'facebook',
+    url: '/facebook.svg',
+  },
+];
