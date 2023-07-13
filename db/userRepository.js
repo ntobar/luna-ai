3df408efdf21564f0aa66ba265e05c8a24239783
Nicolas Tobar <ntobar@bluecedar.com>
2023-07-13 17:30:23 -0400

    fixed uuid

--- db/userRepository.js
+++ db/userRepository.js
@@ -1,15 +1,13 @@
 const db = require('./db');
-const { v4: uuidv4 } = require('uuid');
 
 async function createUser(whatsappNumber, name) {
     try {
-        const userId = uuidv4(); // Generate a new UUID
 
         const result = await db.one(
-            `INSERT INTO users (id, whatsapp_number, name)
-         VALUES ($1, $2, $3)
+            `INSERT INTO users (whatsapp_number, name)
+         VALUES ($1, $2)
          RETURNING id`,
-            [userId, whatsappNumber, name]
+            [whatsappNumber, name]
         );
         await updateLastSeen(result.id)
         console.log(`[ Users Table ] - Successfully created user for ${name} with number ${whatsappNumber}. User Id = ${result.id}`);
