63438328d884343a59310566fe175810375462d7
Nicolas Tobar <ntobar@bluecedar.com>
2023-08-25 18:35:21 -0400

    styling

--- pages/signup.js
+++ pages/signup.js
@@ -94,6 +94,7 @@
 // }
 
 
+// BEST WORKING SOLUTION!!!!
 import { useState } from 'react';
 
 export default function Signup() {
@@ -130,6 +131,52 @@ export default function Signup() {
         {link && (
           <div className="mt-4">
             <a href={link} target="_blank" rel="noopener noreferrer" className="text-blue-500 underline">
+              Open WhatsApp Chat
+            </a>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+// END OF BEST WORKING SOLUTION
+
+import { useState } from 'react';
+
+export default function Signup() {
+  const [email, setEmail] = useState('');
+  const [link, setLink] = useState(null);
+
+  const handleSubmit = async () => {
+    const response = await fetch('/api/generateLink', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email }),
+    });
+
+    const data = await response.json();
+    setLink(data.link);
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gradient-01">
+      <div className="p-8 rounded-lg shadow-lg glassmorphism w-96">
+        <h1 className="text-2xl mb-4 font-bold">Signup for WhatsApp</h1>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Enter your email"
+          className="p-2 w-full mb-4 rounded border border-gray-300 focus:border-blue-500 focus:ring focus:ring-blue-200 focus:outline-none"
+        />
+        <button onClick={handleSubmit} className="w-full p-2 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-200">
+          Get WhatsApp Link
+        </button>
+        {link && (
+          <div className="mt-4">
+            <a href={link} target="_blank" rel="noopener noreferrer" className="text-blue-500 underline hover:text-blue-600">
               Open WhatsApp Chat
             </a>
           </div>
