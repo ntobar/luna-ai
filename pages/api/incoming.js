3d5d5d20d0e0648f2f64cc298dc5be585b6c97d4
Nicolas Tobar <ntobar@bluecedar.com>
2023-09-02 23:38:35 -0400

    switched reset functions

--- pages/api/incoming.js
+++ pages/api/incoming.js
@@ -138,7 +138,8 @@ module.exports = async (req, res) => {
       console.log(`[ CHAT RESET ] User has requested chat reset`);
 
       try {
-        await messageRepository.deleteAllMessagesAndConversation(existingUser.id);
+        const conversationId = conversationRepository.getConversationId(existingUser.id);
+        await conversationRepository.deleteConversation(conversationId);
       } catch (err) {
         console.log(`[ ERROR ][ CHAT RESET ] Error Resetting Chat`);
 
