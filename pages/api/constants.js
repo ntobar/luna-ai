f7fc47efbb567d21bdf6861dbc39511ae5f4a148
Nicolas Tobar <ntobar@bluecedar.com>
2023-10-27 11:05:57 -0400

    aa

--- pages/api/constants.js
+++ pages/api/constants.js
@@ -30,9 +30,7 @@ Hang tight, and enjoy this wild ride through the Luna universe!`,
     `Hello {profile}, you've officially entered the Luna-verse...`,
 ];
 export const spanishWelcomeMessage = [
-    `¡Hola {profile}! Bienvenido oficialmente al mundo de Luna, donde tengo más trucos bajo la manga que David Blaine. Ya sea que necesites respuestas rápidas, imágenes generadas, transcripciones de audio, o revivir nuestras conversaciones pasadas, Luna está aquí para ti.
-
-Es como tener un superhéroe en tu bolsillo - siempre listo para la acción. ¡Prepárate para la aventura!`,
+    `¡Hola {profile}! Bienvenido a Luna, tengo muchas herramientas útiles para ofrecer. Puedo proporcionar respuestas rápidas, crear imágenes, transcribir audio o recordar conversaciones pasadas. Es como tener a un superhéroe en tu bolsillo. ¡Estoy aquí para asistirte!`,
 
     `¡Hola {profile}! Te doy la bienvenida oficial al Lunaverso, donde los chatbots son tan astutos que te harán cuestionar tus propias habilidades. ¿Necesitas respuestas, imágenes generadas, audios transcritos, o revivir nuestras antiguas conversaciones? ¡Luna lo tiene todo!
 
