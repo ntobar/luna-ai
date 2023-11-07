b41abd35d34d75cbda9d68f365147972bf2f0551
Nicolas Tobar <ntobar@bluecedar.com>
2023-11-07 12:33:41 -0500

    trying vision 2222

--- pages/api/incomingContext.js
+++ pages/api/incomingContext.js
@@ -687,7 +687,9 @@ async function visionApi(mediaUrl, prompt) {
       });
 
       console.log("VISION RESPONSE: ", response);
+      console.log("VISION RESPONSE CHOICES: ", JSON.stringify(response.choices));
       console.log("VISION RESPONSE CHOICES: ", JSON.stringify(response.choices[0]));
+
 
       return response.choices[0];
 
