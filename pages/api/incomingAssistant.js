f0248b3e0baebd484bea051db041437e3de69e49
Nicolas Tobar <ntobar@bluecedar.com>
2023-12-24 23:45:53 -0500

    added mutliple tool call handling2343342

--- pages/api/incomingAssistant.js
+++ pages/api/incomingAssistant.js
@@ -1590,9 +1590,7 @@ async function handleMessage(userId, userMessage, mediaUrl, mediaType, profileNa
         // let assistant_id = 'asst_Br2fpSsagEd3LcYCP9fp2RQy';
 
 
-        assistant_id = 'asst_GbXeYwbPuTrkya5nvilPImwQ';
-        let assistant = await setupAssistant();
-        assistant_id = assistant.id;
+        assistant_id = 'asst_qLmHTrTFleZglwQvkznpi2uK';
         console.log(`Assistand ID: ${assistant_id}`);
         // Hard-coded assistant so we dont create a new one for each request
         // const assistant_id = 'asst_0O5Aqevvkh7EufeUeZgYJiHJ';
